36526ff14327499ff3203b5142793969f820e7d9
Corey Davenport <cairnelan@gmail.com>
2016-01-18 13:56:03 -0500

    Testing out grunt concat

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,13 @@
 module.exports = function (grunt) {
-
+	// Project configuration. 
+grunt.initConfig({
+  concat: { 
+    dist: {
+      src: ['js/1.js', 'js/2.js'],
+      dest: 'build/js/scripts.js',
+    },
+  },
+});
 	grunt.loadNpmTasks('grunt-contrib-concat');
 
 };
